f62fadf822105d827dde9408f98a7db4ef9c4ae1
imSoly <imsoly@gmail.com>
2021-10-17 15:59:48 +0900

    Remove Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Header from './Header/Header.jsx';
-import Main from './Main/Main.jsx';
-import Notice from './Notice/Notice.jsx';
-import Payment from './Payment/Payment.jsx';
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Main from "./Main/Main.jsx";
+import Notice from "./Notice/Notice.jsx";
+import Payment from "./Payment/Payment.jsx";
 
 function App() {
-
-  let [communityTitle, setcommunityTitle] = useState(['배드민턴이 너무 어려워요....', '이 배드민채 어떤가요?', '체육관 너무 좋네요^^', '배린이 임니다 ㅎ,,']);
-  let [notice, setnotice] = useState(['배드민턴은 SIPANG...', '으음~ 머시따~!', '내 집 같은 체육관은 ...', 'SIPANG 예약 할인 이벤트!']);
+  let [communityTitle, setcommunityTitle] = useState([
+    "배드민턴이 너무 어려워요....",
+    "이 배드민채 어떤가요?",
+    "체육관 너무 좋네요^^",
+    "배린이 임니다 ㅎ,,",
+  ]);
+  let [notice, setnotice] = useState([
+    "배드민턴은 SIPANG...",
+    "으음~ 머시따~!",
+    "내 집 같은 체육관은 ...",
+    "SIPANG 예약 할인 이벤트!",
+  ]);
 
   return (
     <>
-      <Header /> 
-      
+      <Header />
+
       <Switch>
         <Route exact path="/">
-          <Main communityTitle={communityTitle} notice={notice}/>
+          <Main communityTitle={communityTitle} notice={notice} />
         </Route>
 
         <Route path="/Payment">
@@ -27,11 +37,13 @@ function App() {
         <Route path="/Notice">
           <Notice />
         </Route>
+
+        <Route path="/login">{/* <LoginBox /> */}</Route>
+        <Route path="/join">{/* <JoinBox /> */}</Route>
+        <Route path="/search">{/* <SearchBox /> */}</Route>
       </Switch>
 
-      <div className="footer">
-        
-      </div>
+      <Footer />
     </>
   );
 }
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import './Main.css';
+import React, { useState, useEffect } from "react";
+import "./Main.css";
 
 function Main(props) {
-
   const rankingData = ["성남체육관", "압구정체육관", "신사체육관"];
   const [ranking, setRanking] = useState(rankingData[0]);
 
@@ -10,30 +9,28 @@ function Main(props) {
     useEffect(() => {
       const id = setInterval(() => {
         let tmp = rankingData.indexOf(ranking);
-        let changeIndex = rankingData.length - 1 === tmp 
-        ? 0 
-        : tmp + 1;
+        let changeIndex = rankingData.length - 1 === tmp ? 0 : tmp + 1;
         setRanking(rankingData[changeIndex]);
       }, 3000);
       return () => clearInterval(id);
     }, []);
 
-    return <h1> {rankingData.indexOf(ranking)}. {ranking}</h1>;
+    return <h1>{`${rankingData.indexOf(ranking) + 1}. ${ranking}`}</h1>;
   };
 
-  return(
-      <>
-        <div className="search">
+  return (
+    <>
+      <div className="search">
         <div className="main_img"></div>
         <div className="main_search">
-          <input placeholder="지역명 / 체육관명을 검색해주세요!"/>
+          <input placeholder="지역명 / 체육관명을 검색해주세요!" />
         </div>
       </div>
 
       <div>
         <h2 className="main_title">HOT 체육관!</h2>
         <div className="main_rank">
-            <h1> <Ranking /></h1>
+          <Ranking />
         </div>
       </div>
 
@@ -55,50 +52,48 @@ function Main(props) {
       </div>
 
       <div className="main_reservation">
-        <div className="main_reservation_text1">
-          ddd
-        </div>
-        <div className="main_reservation_text1">
-          ddd
-        </div>
+        <div className="main_reservation_text1">ddd</div>
+        <div className="main_reservation_text1">ddd</div>
       </div>
 
       <div className="main_twobox">
         <div className="main_community">
           <div className="main_community_title">커뮤니티 새글</div>
           <div className="main_squre">
-            {
-              [1,2,3,4].map(function(a, i){
-                return (
-                  <div className="main_community_content">
-                    <text className="main_community_content_title"> {props.communityTitle[i]}</text>
-                    <text className="main_community_content_time">{i + 1}분 전</text>
-                    <div className="main_line"></div>
-                  </div>
-                )
-              })
-            }
+            {[1, 2, 3, 4].map(function (a, i) {
+              return (
+                <div key={i} className="main_community_content">
+                  <p className="main_community_content_title">
+                    {" "}
+                    {props.communityTitle[i]}
+                  </p>
+                  <p className="main_community_content_time">{i + 1}분 전</p>
+                  <div className="main_line"></div>
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="main_notice">
           <div className="main_notice_title">공지사항</div>
           <div className="main_squre">
-            {
-              [1,2,3,4].map(function(a, i){
-                return (
-                  <div className="main_notice_content">
-                    <text className="main_notice_content_title"> {props.notice[i]}</text>
-                    <text className="main_notice_content_time">{i + 1}분 전</text>
-                    <div className="main_line"></div>
-                  </div>
-                )
-              })
-            }
+            {[1, 2, 3, 4].map(function (a, i) {
+              return (
+                <div key={i} className="main_notice_content">
+                  <p className="main_notice_content_title">
+                    {" "}
+                    {props.notice[i]}
+                  </p>
+                  <p className="main_notice_content_time">{i + 1}분 전</p>
+                  <div className="main_line"></div>
+                </div>
+              );
+            })}
           </div>
         </div>
-    </div>
-      </>
-  )
+      </div>
+    </>
+  );
 }
 
 export default Main;
